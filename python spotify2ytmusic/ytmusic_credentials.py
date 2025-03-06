19c45258e41689ea4b329c21f32e23e80966b61f
LELLO1458 <tittitripel@gmail.com>
2025-03-06 18:35:14 +0100

    Create ytmusic_credentials.py

---
+++ python spotify2ytmusic/ytmusic_credentials.py
@@ -0,0 +1,78 @@
+python -m venv .venv
+.venv\Scripts\activate
+pip install ytmusicapi tk
+:authority:
+music.youtube.com
+:method:
+GET
+:path:
+/
+:scheme:
+https
+accept:
+text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+accept-encoding:
+gzip, deflate, br, zstd
+accept-language:
+it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7
+cache-control:
+max-age=0
+cookie:
+HSID=Am66rML0_j4F0JIBt; SSID=A-sX533_7cQK94WtQ; APISID=jjI4hFQUaRYdB3I2/ADV-yCm70Sg3clPFk; SAPISID=_4DMmqEuju2SvO-S/ATxP-q4FjGoWEwGfR; __Secure-1PAPISID=_4DMmqEuju2SvO-S/ATxP-q4FjGoWEwGfR; __Secure-3PAPISID=_4DMmqEuju2SvO-S/ATxP-q4FjGoWEwGfR; VISITOR_PRIVACY_METADATA=CgJJVBIcEhgSFhMLFBUWFwwYGRobHB0eHw4PIBAREiEgHQ%3D%3D; LOGIN_INFO=AFmmF2swRgIhAJwGQNLP8FpYpnaFe5ej0Zx_FoYqxybJECn4AEX_3bIyAiEA_ck_HJm8rXdRk0m2lRkmkzOkyvcJ03hLraJB9s2ofDo:QUQ3MjNmeXdaMFRmdlpUUVlPRVFyT2VldXZkSHRhWk9uZ2tBX0NwbHR4U3Q3QmNiTVdnLW84VkhMV1ZTNVlFS0Z1Q19TS0ZNUDJEckJnX0ViQXJ2RU9TdWVCdFhUaWg0bGVNc0I2OFduOHM3V3VXNk1ONGJZYjBPQkhwMnFRU0w5VDdrNzB0VkdYejFEd0hCMmt6R284Vmo2M3QzTW5jamhR; PREF=f6=40000000&f7=4100&tz=Europe.Rome&f4=4000000; SID=g.a000tQhRx4tfC9e5o_yPSaHgESxZR7YRr1OgU_pc41ax5JUa7pHv39l6RQHFQRUKXQq-965uUgACgYKAZISARcSFQHGX2Mic4zpcQduwOHwUgMFwsSslRoVAUF8yKpJAQHDmT6yMjo7sr88ANxw0076; __Secure-1PSID=g.a000tQhRx4tfC9e5o_yPSaHgESxZR7YRr1OgU_pc41ax5JUa7pHvajlrJJGIBxjTuPE3GciQIAACgYKAYYSARcSFQHGX2MivOpVniwy8Dw0v6JEebJoiRoVAUF8yKqJfU4CtbvUCwJ_gEdir9xg0076; __Secure-3PSID=g.a000tQhRx4tfC9e5o_yPSaHgESxZR7YRr1OgU_pc41ax5JUa7pHvjvzpPqRMB5iNBaLqPTyiiAACgYKAQMSARcSFQHGX2MiKVfJjTeMZ4l6jSeVE1z5rxoVAUF8yKqk4WI4XczdKl9-lgqb8-cS0076; __Secure-ROLLOUT_TOKEN=CNDI0uzbzd6r0QEQhaWM5KO7iwMY3IfvgcTziwM%3D; YSC=JODK_U0qfyk; _gcl_au=1.1.117487354.1741280362; __Secure-1PSIDTS=sidts-CjIBEJ3XV3DFtndEqENHNCjvtr87EBPgpa31uKSBtuYiLkhqrfXkDS6DToVAbgn-YttN7BAA; __Secure-3PSIDTS=sidts-CjIBEJ3XV3DFtndEqENHNCjvtr87EBPgpa31uKSBtuYiLkhqrfXkDS6DToVAbgn-YttN7BAA; CONSISTENCY=AKreu9sId4BNetJqEdOQoRAdkIVf7h6O0RC_oVH1nM2pTisPAO9eKl4REn4ApdWucq3pA1nZ3nbrxj8V8Bp5NmoRsLXyWfpf8HaIs2F_d_2OhxsXhTl9ADg8QLJ351u6XUUuguTET4_i5EMnUCNKw76E; __Secure-YEC=CgtvYWZnWUYtNVRMNCi4t6e-BjIiCgJJVBIcEhgSFhMLFBUWFwwYGRobHB0eHw4PIBAREiEgHQ%3D%3D; SIDCC=AKEyXzUnWL0-Ab-1gUYnMThczkbVIl0OtV8oECHZClszdahhNAyyG0DRiPJsruxgDu4di3SUWTA; __Secure-1PSIDCC=AKEyXzWvy23sg8XWvEHSS-qViiKDmPABGYse0SjFZ0dpM70G93DhRcP8WH8aOZ9ES9mGR1DICw; __Secure-3PSIDCC=AKEyXzUAswaGChePXxrQpnYGfXztjLglBy9ppAgyboriePqHeXefAMO2k2C3m8NQ41Hg4ASpkw
+priority:
+u=0, i
+referer:
+https://www.google.com/
+sec-ch-ua:
+"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"
+sec-ch-ua-arch:
+"x86"
+sec-ch-ua-bitness:
+"64"
+sec-ch-ua-form-factors:
+"Desktop"
+sec-ch-ua-full-version:
+"133.0.6943.142"
+sec-ch-ua-full-version-list:
+"Not(A:Brand";v="99.0.0.0", "Google Chrome";v="133.0.6943.142", "Chromium";v="133.0.6943.142"
+sec-ch-ua-mobile:
+?0
+sec-ch-ua-model:
+""
+sec-ch-ua-platform:
+"Windows"
+sec-ch-ua-platform-version:
+"14.0.0"
+sec-ch-ua-wow64:
+?0
+sec-fetch-dest:
+document
+sec-fetch-mode:
+navigate
+sec-fetch-site:
+same-origin
+sec-fetch-user:
+?1
+service-worker-navigation-preload:
+true
+upgrade-insecure-requests:
+1
+user-agent:
+Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36
+x-browser-channel:
+stable
+x-browser-copyright:
+Copyright 2025 Google LLC. All rights reserved.
+x-browser-validation:
+1nAW9Rb/M8Lkk97ILDg00FWYjns=
+x-browser-year:
+2025
+x-client-data:
+CIu2yQEIorbJAQipncoBCMSAywEIk6HLAQiKo8sBCIWgzQEIuMjNAQiA1s4BCK7kzgEIjOXOARjAy8wBGOHizgE=
+Decodificate:
+message ClientVariations {
+  // Active Google-visible variation IDs on this client. These are reported for analysis, but do not directly affect any server-side behavior.
+  repeated int32 variation_id = [3300107, 3300130, 3313321, 3326020, 3330195, 3330442, 3362821, 3367992, 3386112, 3387950, 3388044];
+  // Active Google-visible variation IDs on this client that trigger server-side behavior. These are reported for analysis *and* directly affect server-side behavior.
+  repeated int32 trigger_variation_id = [3352000, 3387745];
+}
